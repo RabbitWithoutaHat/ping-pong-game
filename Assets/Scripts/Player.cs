599c6f0b727f4b77b1b8172a2ab0205137ee39c0
aleksandra volobueva <Aleksandra.Volobueva@glidewelldental.com>
2020-12-22 20:48:30 +0300

    add player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _JumpPower = 10;
+    public Rigidbody2D Rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+            Rigidbody2D.velocity = new Vector2(0, _JumpPower);
+        }
+    }
+}
